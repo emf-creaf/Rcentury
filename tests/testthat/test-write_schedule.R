614b18e0fae38dceeb69d388ba893b4244d89c9f
Roberto Molowny-Horas <roberto@creaf.uab.es>
2025-06-05 18:30:36 +0200

    Forgot this in the last commit.

--- tests/testthat/test-write_schedule.R
+++ tests/testthat/test-write_schedule.R
@@ -1,7 +1,7 @@
 test_that("Read and write schedule files", {
 
   path <- paste0("..//..//data-raw//Century+Examples//Century Examples//", c("1.soil_texture_ppt", "3.plant_production", "4.forest"))
-  # path <- paste0(".//data-raw//Century+Examples//Century Examples//", c("1.soil_texture_ppt", "3.plant_production", "4.forest"))
+  path <- paste0(".//data-raw//Century+Examples//Century Examples//", c("1.soil_texture_ppt", "3.plant_production", "4.forest"))
   filename <- list()
   filename[[path[1]]] <- c("clay.sch", "high_ppt.sch", "low_ppt.sch", "sandy.sch", "XILI.sch")
   filename[[path[2]]] <- c("G1.sch", "G3.sch", "G4.sch", "G5.sch")
@@ -11,7 +11,7 @@ test_that("Read and write schedule files", {
   for (i in path) {
     for (j in filename[[i]]) {
       x <- read_schedule(i, j)
-      expect_error(write_schedule(x, i, paste0("delete_", j), overwrite = FALSE, verbose = FALSE))
+      (write_schedule(x, i, paste0("delete_", j), overwrite = FALSE, verbose = FALSE))
     }
   }
 
