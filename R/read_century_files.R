ae37cddeb27128c81981ad683a8c5ac5615d9a7e
Roberto Molowny-Horas <roberto@creaf.uab.es>
2025-06-17 17:32:11 +0200

    A new generic function check_file.R to check whether path and filename are correct.

---
+++ R/read_century_files.R
@@ -0,0 +1,6 @@
+read_century_files <- function(path = path, filename = filename, check_site = TRUE) {
+
+  # Check path and file name are correct.
+  check_file(path, filename, check_site = check_site)
+
+}
---
+++ R/write_weather.R
@@ -0,0 +1,3 @@
+write_weather <- function() {
+
+}
