9b9e79bde71dbc8a03f05423fc7d4875ba21f727
Roberto Molowny-Horas <roberto@creaf.uab.es>
2025-09-18 19:36:21 +0200

    Problem with high_ppt.sch. It does not run in R or on cmd. Check!

--- R/century_run.R
+++ R/century_run.R
@@ -34,46 +34,13 @@ century_run <- function(pathname = pathname, schedule = schedule, name_output = 
 
 
   # Check that '*.exe' file can be found in folder.
-  # x <- sapply(c("century_47.exe", "list100_47.exe"), function(x) file.exists(file.path(pathname, x)))
-  # if (!all(x)) stop(paste("Either ''century_47.exe' or 'list100_47.exe' are missing from folder", pathname))
-
   if (!file.exists(file.path(pathname, "century_47.exe"))) stop(paste("Could not find 'century_47.exe' in folder", pathname))
 
 
-  # # Check whether those files exist in folder 'pathname'.
-  # x <- sapply(names(list_files), function(x) file.exists(file.path(pathname, x)))
-  # if (!all(x)) stop(paste("Files from 'list_files' missing in folder", pathname))
-
-
-  # Check whether the schedule file is in folder.
-  # if (file.exists(file.path(pathname, list_files["schedule"])))
-
-#
-#   fsch <- check_path(schedule, ".sch", verbose = verbose)
-#   fout <- check_path(fileout, ".bin", verbose = verbose)
-#   stopifnot("Could not find 'schedule' file" = file.exists(fsch))
-#   check_overwrite(fout, verbose = verbose)
-#   stopifnot("Could not find 'century_47.exe'" = file.exists(file.path(wd, "century_47.exe")))
-#
-#   if (!file.exists(file.path(path, "century_47.exe"))) stop("Could not find 'century_47.exe' in directory")
-
-
-  # Site and weather files are given with their extensions inside the schedule file.
-  # We look for those names and make sure they exist on disk.
-
-  # There should be a weather file (extension .wth) with the same as the file schedule.
-  # WRONG. THE NAME OF THE SITE AND WEATHER FILES ARE INSIDE THE SCHEDULE FILE WITH .100 AND .wth EXTENSIONS. CHECK!!!!
-  # fwth <- paste0(schedule, ".wth")
-  # if (!is.null(wd)) fwth <- file.path(wd, fwth)
-  # stopifnot("Could not find a valid weather file" = file.exists(fwth))
-
-browser()
   # Move to directory, run century_47.exe and move back.
   wd_old <- getwd()
-  setwd(path_out)
+  setwd(pathname)
   args <- c(schedule, name_output, ifelse(extended, "Y", "N"), "")
-  kk <- paste0(shQuote(path_out), shQuote("century_47.exe"))
-browser()
   out <- system2("century_47.exe", input = args, wait = TRUE, stderr = TRUE, stdout = FALSE)
   setwd(wd_old)
 
