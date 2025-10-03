d4bac70f5b228fd1862e4e03a2c0d96ff3094ec7
Roberto Molowny-Horas <roberto@creaf.uab.es>
2025-10-03 16:50:10 +0200

    run_list.R now works calls "century_47.exe" with system2. New things added to example on uncertainty and sensitivity. Switched to pkgdown.

--- R/run_list.R
+++ R/run_list.R
@@ -15,23 +15,10 @@ run_list <- function(pathname, name_bin, name_lis, name_txt) {
 
   wd_old <- getwd()
   setwd(pathname)
-
-  # out <- system2("list100_47.exe", input = params, wait = TRUE, stderr = TRUE, stdout = FALSE) # Couldn't make it work.
-
-  # # Hints from AI Gemini.
-  #
-  # # Check the operating system
-  # os_is_windows <- .Platform$OS.type == "windows"
-  #
-  # # Define the null device path accordingly
-  # null_device <- if (os_is_windows) "NUL" else "/dev/null"
-  #
-  # # Construct the command to suppress output
-  # suppress_command <- paste0(" > ", null_device, " 2>&1")
-
-  out <- system(paste("list100_47.exe", name_bin, name_lis, name_txt))
+  args <- c(name_bin, name_lis, name_txt)
+  out <- system2("list100_47.exe", args = args, wait = TRUE, stderr = TRUE, stdout = FALSE)
   setwd(wd_old)
-  if (out != 0) warning(paste("Error", out, " in list100_47.exe execution"))
+  if (length(out) > 0) warning(paste("Error", out, " in list100_47.exe execution"))
 
   return(out)
 }
